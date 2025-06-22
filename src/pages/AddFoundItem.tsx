45a8f8ee83c0d96a184d6d4c0ff13fdc81b07020
somchai.kh.dev <somchai.kh.dev@gmail.com>
2025-06-23 00:08:51 +0700

    - เพิ่ม react-hook-form - เพิ่ม ui shadcn

--- src/pages/AddFoundItem.tsx
+++ src/pages/AddFoundItem.tsx
@@ -1,45 +1,97 @@
 import { PhotoIcon } from "@heroicons/react/24/solid";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button"
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+
+const FormSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+});
 
 const AddFoundItem = () => {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      username: "",
+    },
+  });
+
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    console.log("onSubmit", data);
+  }
+
   return (
-    <div>
-      <div className="w-full mb-4">
-        <span className="text-2xl font-medium">เพิ่มรายการของที่พบ</span>
-      </div>
-      <div className="flex flex-wrap gap-4 w-full">
-        <div className="grow-0 xs:grow">
-          <div className="col-span-full">
-            <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
-              <div className="text-center">
-                <PhotoIcon
-                  aria-hidden="true"
-                  className="mx-auto size-12 text-gray-300"
-                />
-                <div className="mt-4 flex text-sm/6 text-gray-600">
-                  <label
-                    htmlFor="file-upload"
-                    className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 focus-within:outline-hidden hover:text-indigo-500"
-                  >
-                    <span>Upload a file</span>
-                    <input
-                      id="file-upload"
-                      name="file-upload"
-                      type="file"
-                      className="sr-only"
-                    />
-                  </label>
-                  <p className="pl-1">or drag and drop</p>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+        <div className="w-full mb-4">
+          <span className="text-2xl font-medium">เพิ่มรายการของที่พบ</span>
+        </div>
+        <div className="flex flex-wrap gap-4 w-full">
+          <div className="grow-0 xs:grow">
+            <div className="col-span-full">
+              <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
+                <div className="text-center">
+                  <PhotoIcon
+                    aria-hidden="true"
+                    className="mx-auto size-12 text-gray-300"
+                  />
+                  <div className="mt-4 flex text-sm/6 text-gray-600">
+                    <label
+                      htmlFor="file-upload"
+                      className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 focus-within:outline-hidden hover:text-indigo-500"
+                    >
+                      <span>Upload a file</span>
+                      <input
+                        id="file-upload"
+                        name="file-upload"
+                        type="file"
+                        className="sr-only"
+                      />
+                    </label>
+                    <p className="pl-1">or drag and drop</p>
+                  </div>
+                  <p className="text-xs/5 text-gray-600">
+                    PNG, JPG, GIF up to 10MB
+                  </p>
                 </div>
-                <p className="text-xs/5 text-gray-600">
-                  PNG, JPG, GIF up to 10MB
-                </p>
               </div>
             </div>
           </div>
+          <div className="grow">
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input placeholder="shadcn" {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    This is your public display name.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+
+          <Button type="submit">Submit</Button>
         </div>
-        <div className="grow"></div>
-      </div>
-    </div>
+      </form>
+    </Form>
   );
 };
 
