5da9579cadf653c04bb582dbb32c5c0ad6204591
somchai.kh.dev <somchai.kh.dev@gmail.com>
2025-06-28 10:40:31 +0700

    เพิ่มหน้า login

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,26 @@
 import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import Home from "./pages/Home";
 import Layout from "./pages/Layout";
 import AddFoundItem from "./pages/AddFoundItem";
 import AddLostItem from "./pages/AddLostItem";
+import Login from "./pages/Login";
 
 const App = () => {
-  return (
-    <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/add-found-item" element={<AddFoundItem />} />
-          <Route path="/add-lost-item" element={<AddLostItem />} />
-        </Routes>
-      </Layout>
-    </BrowserRouter>
+  let location = useLocation();
+  
+  return location.pathname !== "/login" ? (
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/add-found-item" element={<AddFoundItem />} />
+        <Route path="/add-lost-item" element={<AddLostItem />} />
+      </Routes>
+    </Layout>
+  ) : (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+    </Routes>
   );
 };
 
---
+++ src/pages/Login.tsx
@@ -0,0 +1,175 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Checkbox from "@mui/material/Checkbox";
+import CssBaseline from "@mui/material/CssBaseline";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormLabel from "@mui/material/FormLabel";
+import FormControl from "@mui/material/FormControl";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
+import Stack from "@mui/material/Stack";
+import MuiCard from "@mui/material/Card";
+import { styled } from "@mui/material/styles";
+import { useNavigate } from "react-router-dom";
+
+const Card = styled(MuiCard)(({ theme }) => ({
+  display: "flex",
+  flexDirection: "column",
+  alignSelf: "center",
+  width: "100%",
+  padding: theme.spacing(4),
+  gap: theme.spacing(2),
+  margin: "auto",
+  [theme.breakpoints.up("sm")]: {
+    maxWidth: "450px",
+  },
+  boxShadow:
+    "hsla(220, 30%, 5%, 0.05) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.05) 0px 15px 35px -5px",
+  ...theme.applyStyles("dark", {
+    boxShadow:
+      "hsla(220, 30%, 5%, 0.5) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.08) 0px 15px 35px -5px",
+  }),
+}));
+
+const SignInContainer = styled(Stack)(({ theme }) => ({
+  height: "calc((1 - var(--template-frame-height, 0)) * 100dvh)",
+  minHeight: "100%",
+  padding: theme.spacing(2),
+  [theme.breakpoints.up("sm")]: {
+    padding: theme.spacing(4),
+  },
+  "&::before": {
+    content: '""',
+    display: "block",
+    position: "absolute",
+    zIndex: -1,
+    inset: 0,
+    backgroundImage:
+      "radial-gradient(ellipse at 50% 50%, hsl(210, 100%, 97%), hsl(0, 0%, 100%))",
+    backgroundRepeat: "no-repeat",
+    ...theme.applyStyles("dark", {
+      backgroundImage:
+        "radial-gradient(at 50% 50%, hsla(210, 100%, 16%, 0.5), hsl(220, 30%, 5%))",
+    }),
+  },
+}));
+
+const Login = (props: { disableCustomTheme?: boolean }) => {
+  const navigate = useNavigate();
+
+  const [usernameError, setUsernameError] = React.useState(false);
+  const [usernameErrorMessage, setUsernameErrorMessage] = React.useState("");
+  const [passwordError, setPasswordError] = React.useState(false);
+  const [passwordErrorMessage, setPasswordErrorMessage] = React.useState("");
+
+  const handleSignIn = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (usernameError || passwordError) {
+      return;
+    }
+    const data = new FormData(event.currentTarget);
+
+    const username = data.get("username")?.toString() ?? "";
+    const password = data.get("password")?.toString() ?? "";
+  };
+
+  const validateInputs = () => {
+    const username = document.getElementById("username") as HTMLInputElement;
+    const password = document.getElementById("password") as HTMLInputElement;
+
+    let isValid = true;
+
+    if (!username.value) {
+      setUsernameError(true);
+      setUsernameErrorMessage("จำเป็นต้องระบุชื่อบัญชี");
+      isValid = false;
+    } else {
+      setUsernameError(false);
+      setUsernameErrorMessage("");
+    }
+
+    if (!password.value || password.value.length < 6) {
+      setPasswordError(true);
+      setPasswordErrorMessage("รหัสผ่านต้องมีไม่ต่ำกว่า 6 ตัว");
+      isValid = false;
+    } else {
+      setPasswordError(false);
+      setPasswordErrorMessage("");
+    }
+
+    return isValid;
+  };
+
+  return (
+    <>
+      <CssBaseline enableColorScheme />
+      <SignInContainer direction="column" justifyContent="space-between">
+        <Card variant="outlined">
+          <Typography
+            component="h1"
+            variant="h4"
+            sx={{ width: "100%", fontSize: "clamp(2rem, 10vw, 2.15rem)" }}
+          >
+            เข้าสู่ระบบเพื่อใช้งาน
+          </Typography>
+          <Box
+            component="form"
+            onSubmit={handleSignIn}
+            noValidate
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              width: "100%",
+              gap: 2,
+            }}
+          >
+            <FormControl>
+              <FormLabel htmlFor="username">ชื่อบัญชี</FormLabel>
+              <TextField
+                error={usernameError}
+                helperText={usernameErrorMessage}
+                id="username"
+                type="username"
+                name="username"
+                autoComplete="off"
+                autoFocus
+                required
+                fullWidth
+                variant="outlined"
+                color={usernameError ? "error" : "primary"}
+              />
+            </FormControl>
+            <FormControl>
+              <FormLabel htmlFor="password">รหัสผ่าน</FormLabel>
+              <TextField
+                error={passwordError}
+                helperText={passwordErrorMessage}
+                name="password"
+                placeholder="••••••"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                // autoFocus
+                required
+                fullWidth
+                variant="outlined"
+                color={passwordError ? "error" : "primary"}
+              />
+            </FormControl>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              onClick={validateInputs}
+            >
+              เข้าสู่ระบบ
+            </Button>
+          </Box>
+        </Card>
+      </SignInContainer>
+    </>
+  );
+};
+
+export default Login;
