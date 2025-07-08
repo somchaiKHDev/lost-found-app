ff95e3db813d5312c4c74f1b9c86e9dd8c725779
somchai.kh.dev <somchai.kh.dev@gmail.com>
2025-07-09 00:26:10 +0700

    comment StrictMode

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,12 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>
+  // <StrictMode>
+  //   <BrowserRouter>
+  //     <App />
+  //   </BrowserRouter>
+  // </StrictMode>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
