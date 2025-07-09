7c0c4b5601c309ea62fc3d9d3ea74a52fddc1ecd
somchai.kh.dev <somchai.kh.dev@gmail.com>
2025-07-10 03:36:54 +0700

    changed

--- src/components/DataPreview/DataPreview.tsx
+++ src/components/DataPreview/DataPreview.tsx
@@ -132,7 +132,7 @@ export const DataPreview = () => {
             </Grid>
           </Grid>
         </Grid>
-        <CampainPreview />
+        {/* <CampainPreview /> */}
       </Box>
     </>
   );
